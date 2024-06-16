be6f50b6079393ffe68cf57c29711bb6b1bcd047
Sergo-Shmel <150228006+Sergo-Shmel@users.noreply.github.com>
2024-06-17 00:15:35 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+file = [("/Users/sergejsmelev/Downloads/access.log", ['%h', '%t', '%r', '%>s', '%b'])]
+db_info = {
+            'database': 'project',
+            'user': 'postgres',
+            'password': '12345',
+            'host': '127.0.0.1',
+            'port': '5432'
+}
